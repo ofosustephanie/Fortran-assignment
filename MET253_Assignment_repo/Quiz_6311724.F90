3c076e5e8c24b8723bd3cf148033f5840c2488d3
ofosustephanie <ofosustephanie7@gmail.com>
2026-05-08 16:26:42 +0000

    Update Quiz_6311724.F90
    changes made to Quiz_6311724.F90

--- MET253_Assignment_repo/Quiz_6311724.F90
+++ MET253_Assignment_repo/Quiz_6311724.F90
@@ -1,27 +1,50 @@
 !6311724
+!Ofosu stephanie
 PROGRAM records
 IMPLICIT NONE
-REAL,DIMENSION(10)::a
+INTEGER,DIMENSION(10)::a
+!I changed from real to integer because the scores are whole numbers not decimals
 INTEGER:: i
-(a)=85
-(a)=62
-(a)=45
-(a)=91
-(a)=38
-(a)=74
-(a)=55
-(a)=88
-(a)=61
-(a)=47
+CHARACTER(LEN=1):: grade
+!I added this to declare a variable to score grades like A,B,C,F
+a(1)=85
+a(2)=62
+a(3)=45
+a(4)=91
+a(5)=38
+a(6)=74
+a(7)=55
+a(8)=88
+a(9)=61
+a(10)=47
+!I numbered them to store the 10 students scores in different position of the array
+PRINT'(A)',"Students Scores Grade Remark"
+!This is to print table heading
 do i=1,10
-if (i>80.AND.i=100))
-Print*,disticntion
-else if(i<60.AND.i<79))
-print*,credit
-else if(i<40.AND.i<59))
-print*,pass
-else if(i<0.AND.i<39))
-print*,fail
+if (a(i)>=80.AND.a(i)<=100)THEN
+!It checks if the score is between 80 and 100
+grade='A'
+!It assigns grade A
+Print'(I3,5X,I3,5X,A,5X,A),i,a(i),grade,"distinction"
+!It prints student number,score,grade and remark neatly
+else if(a(i)>=60.AND.a(i)<=79)THEN
+!It checks if the score is between 60 and 79
+grade='B'
+!It assigns grade B
+print'(I3,5X,I3,5X,A,5X,A),i,a(i),grade,"credit"
+!It prints out the formatted output
+else if(a(i)>=40.AND.a(i)<=59)THEN
+!It checks if the score is between 40 and 59
+grade='C'
+!It assigns grade C
+print'(I3,5X,I3,5X,A,5X,A),i,a(i),grade,"pass"
+!It prints out the formatted output
+else if(a(i)>=0.AND.a(i)<=39)THEN
+!It checks if the score is between 0 and 39
+grade='F'
+!It assigns grade F
+print'(I3,5X,I3,5X,A,5X,A),i,a(i),grade,"fail"
+!It prints out the formatted output
 END IF
 end do
 
